48d7617fde9403cf31af20ee15beaa316811d798
irensvm <irens.vm@gmail.com>
2021-06-18 17:43:07 +0200

    showingblocks

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ kaboom({
     clearColor: [0, 0, 0, 1]
 })
 
-loadRoot('https://imgur.com/')
+loadRoot('https://i.imgur.com/')
 loadSprite('coin', 'wbKxhcd.png')
 loadSprite('evil-shroom', 'KPO3fR9.png')
 loadSprite('brick', 'pogC9x5.png') 
@@ -14,6 +14,11 @@ loadSprite('block', 'bdrLpi6.png')
 loadSprite('mario', 'Wb1qfhK.png') 
 loadSprite('mushroom', '0wMd92p.png') 
 loadSprite('surprise', 'gesQ1KP.png') 
+loadSprite('unboxed', 'bdrLpi6.png') 
+loadSprite('pipe-top-left', 'hj2GK4n.png') 
+loadSprite('pipe-top-right', 'nqQ79eI.png') 
+loadSprite('pipe-bottom-left', 'c1cYSbt.png') 
+loadSprite('pipe-bottom-right', 'nqQ79eI.png') 
 
 
 
@@ -24,10 +29,28 @@ loadSprite('surprise', 'gesQ1KP.png')
 
 
 
-screen("game", () => {
+
+scene("game", () => {
     layers(['bg', 'obj', 'ui'], 'obj')
 
+    const map = [
+        '                                           ',
+        '                                           ',
+        '                                           ',
+        '                                           ',
+        '                                           ',
+        '                                           ',
+        '===================================   =====' 
 
+    ]
+
+    const levelCfg = {
+        width: 20,
+        height:20,
+        '=': [sprite('block', solid())]
+    }
+
+    const gameLevel = addLevel(map, levelCfg)
 
 })
 
